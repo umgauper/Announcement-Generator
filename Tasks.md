dbea6c6f570a67331cebced4f23b4df30d438fe2
Una Gauper <umgauper@gmail.com>
2014-08-29 08:36:28 -0700

    fixed an error

--- Tasks.md
+++ Tasks.md
@@ -1,7 +1,7 @@
 TASKS
 =============
-*use #each helper to list navMarkers
-*call authorSpelled and store what it returns in outputs object
-*Reference notes included/ not included shouldn't have two messages in the output form
-*add client-side validation
-*store input values so user can go back and correct errors
+
+* ~~call authorSpelled and store what it returns in outputs object~~
+* ~~Reference notes included/ not included shouldn't have two messages in the output form~~
+* add client-side validation
+* store input values so user can go back and correct errors
--- js/annGen.js
+++ js/annGen.js
@@ -22,64 +22,69 @@ $(function () {
         $(".right").css("display", "inline");
         $(".right").show("slide", { direction: "left" }, 'slow');
     });
-    function spellOut(name) {
-        var authorSpelled = "";
-        name = name.toString();
-        for(var j=0; j<(name.length-1); j++){
-            authorSpelled += name.charAt(j).toUpperCase() + "-";
-        }
-        authorSpelled += name.charAt(name.length-1).toUpperCase() + "  ";
-    };
-	$(".submit").on('click', function() {
 
+	$(".submit").on('click', function() {
+        function spellOut(name) {
+            var authorSpelled = "";
+            name = name.toString();
+            for(var j=0; j<(name.length-1); j++){
+                authorSpelled += name.charAt(j).toUpperCase() + "-";
+            }
+            authorSpelled += name.charAt(name.length-1).toUpperCase() + "  ";
+            return authorSpelled;
+        }
+
+
+        var navMarkers = [];
+        $("input[type=checkbox]:checked").each(function() {
+            navMarkers.push($(this).val());
+        });
+        var x = "";
+        if(navMarkers.length > 0) {
+            for (var i = 0; i < navMarkers.length; i++) {
+                x = x + navMarkers[i] + ", "
+            }
+            x+="and";
+        }
         var outputs = {
             title: $(".title").val(),
             DBC: $(".DBC").val(),
             authorFirst: $(".author.firstname").val(),
             authorLast: $(".author.lastname").val(),
+            aS:  spellOut( $(".author.firstname").val()) + spellOut($(".author.lastname").val()),
             narratorFirst: $(".narrator.firstname").val(),
             narratorLast: $(".narrator.lastname").val(),
             reviewerFirst: $(".reviewer.firstname").val(),
             reviewerLast: $(".reviewer.lastname").val(),
             copyrightYear: $(".copyright.year").val(),
             copyrightHolder: $(".copyright.holder").val(),
+            nMs:x,
             pages: $(".pages").val(),
             hours: $(".hours").val(),
             minutes: $(".minutes").val(),
-            navMarkers: [],
-            ToC: $("input[type=radio]:checked").filter(".ToC").val(),
             level: $(".level option").filter(":selected").val(),
             refNotes: $("input[name=notes]").filter(":checked").val(),
             refNotesIncl: $("input[name=notes2]").filter(":checked").val(),
             refNotesLoc: $(".refNotesLoc option").filter(":selected").val()
         };
-    $("input[type=checkbox]:checked").each(function() {
-        outputs.navMarkers.push($(this).val());
-    });
-	var x = "";
-    if(outputs.navMarkers.length > 0) {
-        for (var i = 0; i < outputs.navMarkers.length; i++) {
-            x = x + outputs.navMarkers[i] + ", "
-        }
-        x+="and";
-    }
-	if(outputs.ToC == "no") {$(".ToC span").hide()}
-	if(outputs.refNotes=="no") {
-        $(".incl").hide();
-        $(".none").hide();
-    }
-    if(outputs.refNotes=="yes" && outputs.refNotesIncl && outputs.refNotesIncl == "no") {
-        $(".incl").hide();
-    }
-    if(outputs.refNotesIncl == "yes"){
-        $(".none").hide();
-        $(".incl span").text(outputs.refNotesLoc);
-    }
 
-	$("#input").remove();
+
+    $("#input").remove();
 	$("#output").fadeIn('fast');
         var theTemplateScript = $("#myTemplate").html();
         var theTemplate = Handlebars.compile(theTemplateScript);
         $(document.body).append(theTemplate(outputs));
+        if(outputs.refNotes=="no") {
+            $(".incl").hide();
+            $(".none").hide();
+        }
+        if(outputs.refNotes=="yes" && outputs.refNotesIncl && outputs.refNotesIncl == "no") {
+            $(".incl").hide();
+        }
+        if(outputs.refNotesIncl == "yes"){
+            $(".none").hide();
+            $(".incl span").text(outputs.refNotesLoc);
+        }
+
     });
 });
